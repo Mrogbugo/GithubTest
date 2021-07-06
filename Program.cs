35b1519444ba72c58c396436ed120bc7775f7660
Mrogbugo <78213961+Mrogbugo@users.noreply.github.com>
2021-07-06 12:17:17 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unboxValueType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int three = 3;
+            int threeagain = 3;
+
+            bool intCmp = (three == threeagain);
+            Console.WriteLine(string.Format("Compare ints: {0}", intCmp));
+
+            /* this code will evalualte two integer store three and threeagain to know  
+             * if there are equal which will print true on the cmd*/
+
+            bool Cmpobj = ((object)three == (object)threeagain);
+            Console.WriteLine(string.Format("Compare object: {0}", Cmpobj));
+            /* this code will return False because both variable have been cast to object 
+             * which is reference type  
+             * casting valutype to object boxes them then == does reference type*/
+
+            bool itfcmp = ((IComparable<int>)three == (IComparable < int > )threeagain);
+            Console.WriteLine(string.Format("compare interface: {0}", itfcmp));
+            
+           /* this cast both integer to icomparable int, in dot net interface referrance is always 
+            referrance type so this involes boxing too*/  
+
+            ghggj
+
+        }
+    }
+}
